22d086effd28c2668b44c042d74067b472dfef78
Mariusz P <bjei4o@gmail.com>
2025-06-23 13:52:18 +0200

    Update streamlit_app.py
    Added nutrition information for selected fruits

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,17 +26,14 @@ ingredients_list = st.multiselect(
 
 name_on_order = st.text_input('Name on Smoothie: ')
 
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-#st.text(smoothiefroot_response.json())
-sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
-
 if ingredients_list:
-
     ingredients_string = ''
+  
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
-
-    #st.write(ingredients_string)
+      st.subheader(fruit_chosen + 'Nutrition information')
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_chosen)
+        sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
             values (
